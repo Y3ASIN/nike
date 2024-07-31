1a16677ca6086975f237b144e67a7f6a67bc42d3
Yeasin Arafat <yeasinarafat@Yeasins-Air.local>
2024-07-31 15:53:39 +0600

    Project Setup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
-function App() {
-  return <div className="text-4xl text-red-600">Hello world!</div>;
-}
+import {
+  Hero,
+  CustomerReviews,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  Footer,
+} from "./sections";
+import Nav from "./components/Nav";
+
+const App = () => (
+  <main className="relative">
+    <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      {" "}
+      <Hero />{" "}
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="padding bg-pale-blue">
+      {" "}
+      <CustomerReviews />{" "}
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+  </main>
+);
 
 export default App;
