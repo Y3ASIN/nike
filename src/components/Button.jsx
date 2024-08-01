adfcc515768190bda22581867c62a46e303345ef
Yeasin Arafat <yeasinarafat@Yeasins-Air.local>
2024-08-01 20:54:37 +0600

    Feature: Uploading SuperQuality.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ label, iconURL }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg bg-coral-red rounded-full text-white border-coral-red">
       {label}
-      <img src={iconURL} className="ml-2 rounded-full h-5 w-5" />
+      {iconURL && <img src={iconURL} className="ml-2 rounded-full h-5 w-5" />}
     </button>
   );
 };
