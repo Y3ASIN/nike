66ac7ed6df6766a8efb0a0808bb68d94e091c74c
Yeasin Arafat <yeasinarafat@Yeasins-Air.local>
2024-08-01 23:35:54 +0600

    Update: Styled Review section.

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,10 +1,25 @@
+import ReviewCard from "../components/ReviewCard";
+import { reviews } from "../constants";
 
 const CustomerReviews = () => {
   return (
-    <div>
-      Customer Reviews
-    </div>
-  )
-}
+    <section className="max-container">
+      <h3 className="font-palanquin text-center font-bold text-4xl">
+        What Our
+        <span className="text-coral-red"> Customer </span>
+        Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Here genuine stories from our satisfies customers about their
+        exceptional experiences with us.
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard key={review.customerName} {...review} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default CustomerReviews
+export default CustomerReviews;
