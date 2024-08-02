2aa8ccb9122c2aaf363f952ac0d0824cf37e0215
Yeasin Arafat <yeasinarafat@Yeasins-Air.local>
2024-08-02 18:50:49 +0600

    Added: Special feature texts.

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -20,14 +20,23 @@ const SpecialOffer = () => {
           Offer
         </h2>
         <p className="mt-4 lg:max-w-lg info-text">
-          Embark on shopping journey that redefines your experience with unbeatable deals. From premier selections to incredible savings, we offer unparalleled value that sets us apart.
+          Embark on shopping journey that redefines your experience with
+          unbeatable deals. From premier selections to incredible savings, we
+          offer unparalleled value that sets us apart.
         </p>
         <p className="mt-6 lg:max-w-lg info-text">
-         Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the loftiest expectations. Your journey with us is nothing short of exceptional.
+          Navigate a realm of possibilities designed to fulfill your unique
+          desires, surpassing the loftiest expectations. Your journey with us is
+          nothing short of exceptional.
         </p>
         <div className="mt-11 flex flex-wrap gap-4">
           <Button label="Shop now" iconURL={arrowRight} />
-          <Button label='Learn more' backgroundColor='bg-white' borderColor='text-slate-gray' textColor='text-slate-gray'/>
+          <Button
+            label="Learn more"
+            backgroundColor="bg-white"
+            borderColor="text-slate-gray"
+            textColor="text-slate-gray"
+          />
         </div>
       </div>
     </section>
